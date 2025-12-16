54524b55f1aabfde798e5b155404c86179a02961
csjSIST <csj9630@gmail.com>
2025-12-16 14:45:27 +0900

    두번째

---
+++ src/jspprj/GitTest.java
@@ -0,0 +1,11 @@
+package jspprj;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Cyclone");
+		System.out.println("Joker");
+	}
+
+}
